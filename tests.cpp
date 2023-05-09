1cc4968af63cc6d59885e447aa98bd54361f59f0
memesnopower <115490701+memesnopower@users.noreply.github.com>
2023-05-09 20:23:59 +0300

    Update tests.cpp

--- tests.cpp
+++ tests.cpp
@@ -1,6 +1,21 @@
 
 #include "CSet.h"
 
+int maxNum(unsigned int* arr, int size) {
+	int key = 0;
+	size_t i = 0;
+	for (size_t j = 1; j < size; j++) {
+		key = arr[j];
+		i = j - 1;
+		while (i >= 0 && arr[i] > key) {
+			arr[i + 1] = arr[i];
+			i = i - 1;
+			arr[i + 1] = key;
+		}
+	}
+	return arr[size - 1];
+}
+
 int main() {
 	setlocale(LC_ALL, "rus");
 
@@ -25,56 +40,18 @@ int main() {
 		std::cin >> arr2[i];
 	}
 
-	CSet ob1(arr1, N);
+	//std::cout << maxNum(arr1, N);
+
+	int max1 = maxNum(arr1, N);
+	int max2 = maxNum(arr2, M);
+	CSet ob1(max1);
 	add(ob1, arr1, N);
 
-	std::cout << "Первое множество: " << std::endl;
 	std::cout << ob1 << std::endl;
-
-	CSet ob2(arr2, M);
+	
+	CSet ob2(max2);
 	add(ob2, arr2, M);
-
-	std::cout << "Второе множество: " << std::endl;
 	std::cout << ob2 << std::endl;
-
-	CSet ob_copy(arr1, N);
-	ob_copy = ob1;
-
-	std::cout << "Конструктор копирования: " << std::endl;
-	std::cout << ob_copy << std::endl;
-
-	CSet ob3(arr1, N);
-	CSet ob_move(arr1, N);
-	add(ob3, arr1, N);
-	ob_move = std::move(ob3);
-
-	std::cout << "Конструктор перемещения: " << std::endl;
-	std::cout << ob_move << std::endl;
-
-	/*CSet ob_merged(arr1, N);
-	CSet ob4(arr1, N);
-	CSet ob5(arr2, M);
-	add(ob4, arr1, N);
-	add(ob5, arr2, M);
-
-	ob_merged = ob4 + ob5;
-
-	std::cout << "Тест operator+ или же объединение множеств: " << std::endl;
-	std::cout << ob_merged << std::endl;
-
-
-
-	CSet ob_intersection(arr1, N);
-	CSet ob6(arr1, N);
-	CSet ob7(arr2, M);
-	add(ob6, arr1, N);
-	add(ob7, arr2, M);
-
-	ob_intersection = ob6 - ob7;
-
-	std::cout << "Тест operator- или же пересечение множеств: " << std::endl;
-	std::cout << ob_intersection << std::endl;*/
-
 
 
 	delete[] arr1;
